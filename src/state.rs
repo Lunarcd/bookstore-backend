a145a2aa8cd4fa654d6a397c5c9681bc859f38a6
Lunarcd <nhatanhth0212@gmail.com>
2025-06-23 14:49:39 +0700

    feat: adding state.rs

---
+++ src/state.rs
@@ -0,0 +1,44 @@
+use std::{collections::HashMap, sync::Arc};
+
+use database::{deadpool_postgres, tokio_postgres::NoTls};
+use futures::{StreamExt, stream};
+
+use crate::{
+    config::{CONFIG, oidc::Provider},
+    util::{jwt::JwtService, oidc::OpenIdConnectClient},
+};
+
+#[allow(unused)]
+pub struct ApiState {
+    pub database_pool: deadpool_postgres::Pool,
+    pub oidc_clients: HashMap<Provider, OpenIdConnectClient>,
+    pub jwt_service: JwtService,
+}
+
+impl ApiState {
+    pub async fn new() -> Arc<Self> {
+        let mut database_config = deadpool_postgres::Config::new();
+        database_config.url = Some(CONFIG.database_url.clone());
+        let database_pool = database_config
+            .create_pool(Some(deadpool_postgres::Runtime::Tokio1), NoTls)
+            .unwrap();
+
+        let oidc_clients = stream::iter(CONFIG.oidc.iter())
+            .then(|(&provider, config)| async move {
+                (
+                    provider,
+                    OpenIdConnectClient::from_config(config.clone())
+                        .await
+                        .unwrap(),
+                )
+            })
+            .collect()
+            .await;
+
+        Arc::new(Self {
+            database_pool,
+            oidc_clients,   
+            jwt_service: Default::default(),
+        })
+    }
+}
